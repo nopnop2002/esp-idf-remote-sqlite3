29ca65173a70f92e3152f1225fbc85a9769d6392
nopnop2002 <hau26280@tree.odn.ne.jp>
2020-10-31 18:42:01 +0900

    First Release

--- README.md
+++ README.md
@@ -1,2 +1,259 @@
 # esp-idf-remote-sqlite3
-Remote-Sqlite access for esp-idf
+Remote-Sqlite access example for esp-idf.   
+You can access Sqlite3 on the server over the network.   
+Use [this](https://github.com/alixaxel/ArrestDB) as PHP script of WEB server.   
+
+![remote-sqlite3](https://user-images.githubusercontent.com/6020549/97775795-c21af600-1ba6-11eb-9d02-04dcaca058c7.jpg)
+
+# Server Side
+
+## Create sqlite3 database   
+Run the following script in any directory.   
+```
+#!/bin/bash
+#set -x
+
+if [ ! -x $(which sqlite3) ];then
+  echo "sqlite3 not found."
+  exit -1
+fi
+
+cd $(dirname $0)
+dbfile=$1
+if [ $# -ne 1 ];then
+  dbfile="example.db"
+  echo "Create ${dbfile}"
+fi
+
+
+# Create Database
+if [ ! -e ${dbfile} ];then
+  echo ".open ${dbfile}" | sqlite3
+  echo "build database"
+else
+  echo "database already exists"
+fi
+
+option="-noheader -separator ,"
+sqlite="sqlite3 ${option} ${dbfile} "
+
+# Create Table
+${sqlite} "create table if not exists customers( \
+id integer primary key autoincrement, \
+name varchar(255), \
+gender int);"
+
+# Insert Data
+${sqlite} "insert into customers(name,gender) values(\"Luis\", 1);"
+${sqlite} "insert into customers(name,gender) values(\"Leonie\", 1);"
+${sqlite} "insert into customers(name,gender) values(\"Francois\", 2);"
+${sqlite} "insert into customers(name,gender) values(\"Bjorn\", 2);"
+
+${sqlite} "select * from customers;"
+```
+
+## Install ArrestDB
+```
+$ git close https://github.com/alixaxel/ArrestDB
+$ cd ArrestDB
+$ vi index.php
+
+Set the full path of the database in the following line.
+
+$dsn = '';
+
+For example, if the database is //home/nop/example.db, it will be as follows.
+
+$dsn = 'sqlite:///home/nop/example.db';
+
+```
+
+## Install PHP
+```
+
+$ sudo apt install php
+
+$ php --version
+PHP 7.2.24-0ubuntu0.18.04.1 (cli) (built: Oct 28 2019 12:07:07) ( NTS )
+Copyright (c) 1997-2018 The PHP Group
+Zend Engine v3.2.0, Copyright (c) 1998-2018 Zend Technologies
+    with Zend OPcache v7.2.24-0ubuntu0.18.04.1, Copyright (c) 1999-2018, by Zend Technologies
+```
+
+## Install PDO driver for Sqlite3
+```
+$ sudo apt-get install php[Version]-sqlite3
+```
+
+When the PHP version is 7.2.x, execute the following.   
+```
+$ sudo apt-get install php7.2-sqlite3
+```
+
+
+## Start Built-in WEB Server
+```
+$ php -S 0.0.0.0:8080 -t $HOME/ArrestDB
+PHP 7.2.24-0ubuntu0.18.04.7 Development Server started at Sat Oct 31 18:00:29 2020
+Listening on http://0.0.0.0:8080
+Document root is /home/nop/ArrestDB
+Press Ctrl-C to quit.
+```
+
+
+## Test ArrestDB
+```
+$ curl "http://localhost:8080/customers/"
+[
+    {
+        "id": "1",
+        "name": "Luis",
+        "gender": "1"
+    },
+    {
+        "id": "2",
+        "name": "Leonie",
+        "gender": "1"
+    },
+    {
+        "id": "3",
+        "name": "Francois",
+        "gender": "2"
+    },
+    {
+        "id": "4",
+        "name": "Bjorn",
+        "gender": "2"
+    }
+]
+```
+
+---
+
+# ESP32 Side
+
+
+## Software requirements
+esp-idf ver4.1 or later.   
+
+## Install
+
+```
+git clone https://github.com/nopnop2002/esp-idf-remote-sqlite3
+cd esp-idf-remote-sqlite3/
+make menuconfig
+make flash monitor
+```
+
+You have to set this config value with menuconfig.   
+- CONFIG_ESP_WIFI_SSID   
+SSID of your wifi.
+- CONFIG_ESP_WIFI_PASSWORD   
+PASSWORD of your wifi.
+- CONFIG_ESP_MAXIMUM_RETRY   
+Maximum number of retries when connecting to wifi.
+- CONFIG_ESP_WEB_SERVER_IP   
+IP or DNS of your WEB Server.
+- CONFIG_ESP_WEB_SERVER_PORT   
+Port number of your WEB Server.
+- CONFIG_JSON_PARSE   
+Enable JSON parse.
+
+![menuconfig-1](https://user-images.githubusercontent.com/6020549/97775496-79623d80-1ba4-11eb-99cc-1b309aa1689b.jpg)
+
+![menuconfig-2](https://user-images.githubusercontent.com/6020549/97775498-7bc49780-1ba4-11eb-9d32-1984978f9b8b.jpg)
+
+## Read all data
+```
+I (4557) SQLITE: [
+    {
+        "id": "1",
+        "name": "Luis",
+        "gender": "1"
+    },
+    {
+        "id": "2",
+        "name": "Leonie",
+        "gender": "1"
+    },
+    {
+        "id": "3",
+        "name": "Francois",
+        "gender": "2"
+    },
+    {
+        "id": "4",
+        "name": "Bjorn",
+        "gender": "2"
+    }
+]```
+
+## Read by ID
+```
+I (36197) SQLITE: {
+    "id": "3",
+    "name": "Francois",
+    "gender": "2"
+}
+```
+
+## Read by gender
+```
+I (65607) SQLITE: [
+    {
+        "id": "3",
+        "name": "Francois",
+        "gender": "2"
+    },
+    {
+        "id": "4",
+        "name": "Bjorn",
+        "gender": "2"
+    }
+]
+```
+
+## Create new record
+```
+I (83577) SQLITE: {
+    "id": "5",
+    "name": "Tom",
+    "gender": "1"
+}
+```
+
+## Update new record
+```
+I (100747) SQLITE: {
+    "id": "5",
+    "name": "Petty",
+    "gender": "2"
+}
+```
+
+## Delete new record
+```
+I (140287) SQLITE: [
+    {
+        "id": "1",
+        "name": "Luis",
+        "gender": "1"
+    },
+    {
+        "id": "2",
+        "name": "Leonie",
+        "gender": "1"
+    },
+    {
+        "id": "3",
+        "name": "Francois",
+        "gender": "2"
+    },
+    {
+        "id": "4",
+        "name": "Bjorn",
+        "gender": "2"
+    }
+]
+```
+
---
+++ main/main.c
@@ -0,0 +1,585 @@
+/* The example of ESP-IDF
+ *
+ * This sample code is in the public domain.
+ */
+#include <string.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "freertos/ringbuf.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
+#include "esp_http_client.h" 
+#include "esp_tls.h" 
+#include "cJSON.h"
+
+
+/* The examples use WiFi configuration that you can set via project configuration menu
+
+   If you'd rather not, just change the below entries to strings with
+   the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
+*/
+#define EXAMPLE_ESP_WIFI_SSID			CONFIG_ESP_WIFI_SSID
+#define EXAMPLE_ESP_WIFI_PASS			CONFIG_ESP_WIFI_PASSWORD
+#define EXAMPLE_ESP_MAXIMUM_RETRY		CONFIG_ESP_MAXIMUM_RETRY
+#define EXAMPLE_ESP_WEB_SERVER_IP		CONFIG_ESP_WEB_SERVER_IP
+#define EXAMPLE_ESP_WEB_SERVER_PORT		CONFIG_ESP_WEB_SERVER_PORT
+
+/* FreeRTOS event group to signal when we are connected*/
+static EventGroupHandle_t wifi_event_group;
+
+/* The event group allows multiple bits for each event, but we only care about one event */
+/* - Are we connected to the AP with an IP? */
+const int WIFI_CONNECTED_BIT = BIT0;
+
+
+static const char *TAG = "SQLITE";
+
+static int s_retry_num = 0;
+
+EventGroupHandle_t xEventGroup;
+/* - Is the Enter key entered? */
+const int KEYBOARD_ENTER_BIT = BIT2;
+
+
+esp_err_t _http_event_handler(esp_http_client_event_t *evt)
+{
+	static char *output_buffer;  // Buffer to store response of http request from event handler
+	static int output_len;		 // Stores number of bytes read
+	switch(evt->event_id) {
+		case HTTP_EVENT_ERROR:
+			ESP_LOGD(TAG, "HTTP_EVENT_ERROR");
+			break;
+		case HTTP_EVENT_ON_CONNECTED:
+			ESP_LOGD(TAG, "HTTP_EVENT_ON_CONNECTED");
+			break;
+		case HTTP_EVENT_HEADER_SENT:
+			ESP_LOGD(TAG, "HTTP_EVENT_HEADER_SENT");
+			break;
+		case HTTP_EVENT_ON_HEADER:
+			ESP_LOGD(TAG, "HTTP_EVENT_ON_HEADER, key=%s, value=%s", evt->header_key, evt->header_value);
+			break;
+		case HTTP_EVENT_ON_DATA:
+			ESP_LOGD(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
+			ESP_LOGD(TAG, "HTTP_EVENT_ON_DATA, output_len=%d", output_len);
+			ESP_LOGD(TAG, "HTTP_EVENT_ON_DATA, content_length=%d", esp_http_client_get_content_length(evt->client));
+			// If user_data buffer is configured, copy the response into the buffer
+			if (evt->user_data) {
+				memcpy(evt->user_data + output_len, evt->data, evt->data_len);
+			} else {
+				if (output_buffer == NULL && esp_http_client_get_content_length(evt->client) > 0) {
+					output_buffer = (char *) malloc(esp_http_client_get_content_length(evt->client));
+					output_len = 0;
+					if (output_buffer == NULL) {
+						ESP_LOGE(TAG, "Failed to allocate memory for output buffer");
+						return ESP_FAIL;
+					}
+				}
+				memcpy(output_buffer + output_len, evt->data, evt->data_len);
+			}
+			output_len += evt->data_len;
+			break;
+		case HTTP_EVENT_ON_FINISH:
+			ESP_LOGD(TAG, "HTTP_EVENT_ON_FINISH");
+			if (output_buffer != NULL) {
+				// Response is accumulated in output_buffer. Uncomment the below line to print the accumulated response
+				// ESP_LOG_BUFFER_HEX(TAG, output_buffer, output_len);
+				free(output_buffer);
+				output_buffer = NULL;
+			}
+			output_len = 0;
+			break;
+		case HTTP_EVENT_DISCONNECTED:
+			ESP_LOGD(TAG, "HTTP_EVENT_DISCONNECTED");
+			int mbedtls_err = 0;
+			esp_err_t err = esp_tls_get_and_clear_last_error(evt->data, &mbedtls_err, NULL);
+			if (err != 0) {
+				if (output_buffer != NULL) {
+					free(output_buffer);
+					output_buffer = NULL;
+				}
+				output_len = 0;
+				ESP_LOGE(TAG, "Last esp error code: 0x%x", err);
+				ESP_LOGE(TAG, "Last mbedtls failure: 0x%x", mbedtls_err);
+			}
+			break;
+	}
+	return ESP_OK;
+}
+
+static void event_handler(void* arg, esp_event_base_t event_base,
+								int32_t event_id, void* event_data)
+{
+	if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+		esp_wifi_connect();
+	} else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+		if (s_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY) {
+			esp_wifi_connect();
+			xEventGroupClearBits(wifi_event_group, WIFI_CONNECTED_BIT);
+			s_retry_num++;
+			ESP_LOGI(TAG, "retry to connect to the AP");
+		}
+		ESP_LOGE(TAG,"connect to the AP fail");
+	} else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+		ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+		ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+		//ESP_LOGI(TAG, "got ip:%s", ip4addr_ntoa(&event->ip_info.ip));
+		s_retry_num = 0;
+		xEventGroupSetBits(wifi_event_group, WIFI_CONNECTED_BIT);
+	}
+}
+
+void wifi_init_sta()
+{
+	wifi_event_group = xEventGroupCreate();
+
+#if 0
+	tcpip_adapter_init();
+
+	ESP_ERROR_CHECK(esp_event_loop_create_default());
+#endif
+	ESP_ERROR_CHECK(esp_netif_init());
+	
+	ESP_ERROR_CHECK(esp_event_loop_create_default());
+	esp_netif_create_default_wifi_sta();
+
+	wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+	ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+	ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
+	ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL));
+
+	wifi_config_t wifi_config = {
+		.sta = {
+			.ssid = EXAMPLE_ESP_WIFI_SSID,
+			.password = EXAMPLE_ESP_WIFI_PASS
+		},
+	};
+	ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
+	ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
+	ESP_ERROR_CHECK(esp_wifi_start() );
+
+	xEventGroupWaitBits(wifi_event_group, WIFI_CONNECTED_BIT, pdFALSE, pdTRUE, portMAX_DELAY);
+	ESP_LOGI(TAG, "wifi_init_sta finished.");
+	ESP_LOGI(TAG, "connect to ap SSID:%s", EXAMPLE_ESP_WIFI_SSID);
+}
+
+
+char *JSON_Types(int type) {
+	if (type == cJSON_Invalid) return ("cJSON_Invalid");
+	if (type == cJSON_False) return ("cJSON_False");
+	if (type == cJSON_True) return ("cJSON_True");
+	if (type == cJSON_NULL) return ("cJSON_NULL");
+	if (type == cJSON_Number) return ("cJSON_Number");
+	if (type == cJSON_String) return ("cJSON_String");
+	if (type == cJSON_Array) return ("cJSON_Array");
+	if (type == cJSON_Object) return ("cJSON_Object");
+	if (type == cJSON_Raw) return ("cJSON_Raw");
+	return NULL;
+}
+
+void JSON_Parse(const cJSON * const root) {
+	//ESP_LOGI(TAG, "root->type=%s", JSON_Types(root->type));
+	cJSON *current_element = NULL;
+	//ESP_LOGI(TAG, "root->child=%p", root->child);
+	//ESP_LOGI(TAG, "root->next =%p", root->next);
+	cJSON_ArrayForEach(current_element, root) {
+		//ESP_LOGI(TAG, "type=%s", JSON_Types(current_element->type));
+		//ESP_LOGI(TAG, "current_element->string=%p", current_element->string);
+		if (current_element->string) {
+			const char* string = current_element->string;
+			ESP_LOGI(TAG, "[%s]", string);
+		}
+		if (cJSON_IsInvalid(current_element)) {
+			ESP_LOGI(TAG, "Invalid");
+		} else if (cJSON_IsFalse(current_element)) {
+			ESP_LOGI(TAG, "False");
+		} else if (cJSON_IsTrue(current_element)) {
+			ESP_LOGI(TAG, "True");
+		} else if (cJSON_IsNull(current_element)) {
+			ESP_LOGI(TAG, "Null");
+		} else if (cJSON_IsNumber(current_element)) {
+			int valueint = current_element->valueint;
+			double valuedouble = current_element->valuedouble;
+			ESP_LOGI(TAG, "int=%d double=%f", valueint, valuedouble);
+		} else if (cJSON_IsString(current_element)) {
+			const char* valuestring = current_element->valuestring;
+			ESP_LOGI(TAG, "%s", valuestring);
+		} else if (cJSON_IsArray(current_element)) {
+			ESP_LOGD(TAG, "Array");
+			JSON_Parse(current_element);
+		} else if (cJSON_IsObject(current_element)) {
+			ESP_LOGD(TAG, "Object");
+			JSON_Parse(current_element);
+		} else if (cJSON_IsRaw(current_element)) {
+			ESP_LOGI(TAG, "Raw(Not support)");
+		}
+	}
+}
+
+#define MAX_HTTP_OUTPUT_BUFFER 2048
+
+esp_err_t sqlite3_client_get(char * path)
+{
+	ESP_LOGI(TAG, "sqlite3_client_get path=%s",path);
+	char local_response_buffer[MAX_HTTP_OUTPUT_BUFFER] = {0};
+	char url[64];
+	sprintf(url, "http://%s:%s/", EXAMPLE_ESP_WEB_SERVER_IP, EXAMPLE_ESP_WEB_SERVER_PORT);
+	if (strlen(path) > 0) {
+		int url_length = strlen(url);
+		if (url[url_length-1] != '/') strcat(url,"/");
+		strcat(url, path);
+	}
+	ESP_LOGI(TAG, "url=%s",url);
+	
+	esp_http_client_config_t config = {
+		.url = url,
+		.event_handler = _http_event_handler,
+		.user_data = local_response_buffer,		   // Pass address of local buffer to get response
+	};
+	esp_http_client_handle_t client = esp_http_client_init(&config);
+
+	// GET
+	esp_err_t ret;
+	esp_err_t err = esp_http_client_perform(client);
+	if (err == ESP_OK) {
+		ESP_LOGD(TAG, "HTTP GET Status = %d, content_length = %d",
+				esp_http_client_get_status_code(client),
+				esp_http_client_get_content_length(client));
+		ESP_LOGD(TAG, "local_response_buffer=%d",  strlen(local_response_buffer));
+		//ESP_LOG_BUFFER_HEX(TAG, local_response_buffer, strlen(local_response_buffer));
+		ESP_LOGI(TAG, "%s",  local_response_buffer);
+
+#if CONFIG_JSON_PARSE
+		ESP_LOGI(TAG, "Deserialize.....");
+		cJSON *root = cJSON_Parse(local_response_buffer);
+		JSON_Parse(root);
+		cJSON_Delete(root);
+#endif
+		ret = ESP_OK;
+	} else {
+		ESP_LOGE(TAG, "HTTP GET request failed: %s", esp_err_to_name(err));
+		ret = ESP_FAIL;
+	}
+	esp_http_client_cleanup(client);
+	return ret;
+}
+
+esp_err_t sqlite3_client_post(char * path, char * name, int gender)
+{
+	ESP_LOGI(TAG, "sqlite3_client_post path=%s",path);
+	char local_response_buffer[MAX_HTTP_OUTPUT_BUFFER] = {0};
+	char url[64];
+	sprintf(url, "http://%s:%s/", EXAMPLE_ESP_WEB_SERVER_IP, EXAMPLE_ESP_WEB_SERVER_PORT);
+	if (strlen(path) > 0) {
+		int url_length = strlen(url);
+		if (url[url_length-1] != '/') strcat(url,"/");
+		strcat(url, path);
+	}
+	ESP_LOGI(TAG, "url=%s",url);
+	
+	esp_http_client_config_t config = {
+		.url = url,
+		.event_handler = _http_event_handler,
+		.user_data = local_response_buffer,		   // Pass address of local buffer to get response
+	};
+	esp_http_client_handle_t client = esp_http_client_init(&config);
+
+	// POST
+	char post_data[64];
+	sprintf(post_data, "name=%s&gender=%d", name, gender);
+	ESP_LOGI(TAG, "post_data=[%s]", post_data);
+	esp_http_client_set_url(client, url);
+	esp_http_client_set_method(client, HTTP_METHOD_POST);
+	esp_http_client_set_post_field(client, post_data, strlen(post_data));
+	esp_err_t ret;
+	esp_err_t err = esp_http_client_perform(client);
+	if (err == ESP_OK) {
+		ESP_LOGD(TAG, "HTTP GET Status = %d, content_length = %d",
+				esp_http_client_get_status_code(client),
+				esp_http_client_get_content_length(client));
+		ESP_LOGD(TAG, "local_response_buffer=%d",  strlen(local_response_buffer));
+		//ESP_LOG_BUFFER_HEX(TAG, local_response_buffer, strlen(local_response_buffer));
+		ESP_LOGI(TAG, "%s",  local_response_buffer);
+
+#if CONFIG_JSON_PARSE
+		ESP_LOGI(TAG, "Deserialize.....");
+		cJSON *root = cJSON_Parse(local_response_buffer);
+		JSON_Parse(root);
+		cJSON_Delete(root);
+#endif
+		ret = ESP_OK;
+		
+	} else {
+		ESP_LOGE(TAG, "HTTP POST request failed: %s", esp_err_to_name(err));
+	}
+	esp_http_client_cleanup(client);
+	return ret;
+}
+
+esp_err_t sqlite3_client_put(char * path, char * name, int gender)
+{
+	ESP_LOGI(TAG, "sqlite3_client_put path=%s",path);
+	char local_response_buffer[MAX_HTTP_OUTPUT_BUFFER] = {0};
+	char url[64];
+	sprintf(url, "http://%s:%s/", EXAMPLE_ESP_WEB_SERVER_IP, EXAMPLE_ESP_WEB_SERVER_PORT);
+	if (strlen(path) > 0) {
+		int url_length = strlen(url);
+		if (url[url_length-1] != '/') strcat(url,"/");
+		strcat(url, path);
+	}
+	ESP_LOGI(TAG, "url=%s",url);
+
+	esp_http_client_config_t config = {
+		.url = url,
+		.event_handler = _http_event_handler,
+		.user_data = local_response_buffer,		   // Pass address of local buffer to get response
+	};
+	esp_http_client_handle_t client = esp_http_client_init(&config);
+
+	// PUT
+	char post_data[64];
+	sprintf(post_data, "name=%s&gender=%d", name, gender);
+	ESP_LOGI(TAG, "post_data=[%s]", post_data);
+	esp_http_client_set_url(client, url);
+	esp_http_client_set_method(client, HTTP_METHOD_PUT);
+	esp_http_client_set_post_field(client, post_data, strlen(post_data));
+	esp_err_t ret;
+	esp_err_t err = esp_http_client_perform(client);
+	if (err == ESP_OK) {
+		ESP_LOGD(TAG, "HTTP GET Status = %d, content_length = %d",
+				esp_http_client_get_status_code(client),
+				esp_http_client_get_content_length(client));
+		ESP_LOGD(TAG, "local_response_buffer=%d",  strlen(local_response_buffer));
+		//ESP_LOG_BUFFER_HEX(TAG, local_response_buffer, strlen(local_response_buffer));
+		ESP_LOGI(TAG, "%s",  local_response_buffer);
+
+#if CONFIG_JSON_PARSE
+		ESP_LOGI(TAG, "Deserialize.....");
+		cJSON *root = cJSON_Parse(local_response_buffer);
+		JSON_Parse(root);
+		cJSON_Delete(root);
+#endif
+		ret = ESP_OK;
+	} else {
+		ESP_LOGE(TAG, "HTTP PUT request failed: %s", esp_err_to_name(err));
+		ret = ESP_FAIL;
+	}
+	esp_http_client_cleanup(client);
+	return ret;
+}
+
+esp_err_t sqlite3_client_delete(char * path)
+{
+	ESP_LOGI(TAG, "sqlite3_client_delete path=%s",path);
+	char local_response_buffer[MAX_HTTP_OUTPUT_BUFFER] = {0};
+	char url[64];
+	sprintf(url, "http://%s:%s/", EXAMPLE_ESP_WEB_SERVER_IP, EXAMPLE_ESP_WEB_SERVER_PORT);
+	if (strlen(path) > 0) {
+		int url_length = strlen(url);
+		if (url[url_length-1] != '/') strcat(url,"/");
+		strcat(url, path);
+	}
+	ESP_LOGI(TAG, "url=%s",url);
+
+	esp_http_client_config_t config = {
+		.url = url,
+		.event_handler = _http_event_handler,
+		.user_data = local_response_buffer,		   // Pass address of local buffer to get response
+	};
+	esp_http_client_handle_t client = esp_http_client_init(&config);
+
+	// DELETE
+	esp_http_client_set_url(client, url);
+	esp_http_client_set_method(client, HTTP_METHOD_DELETE);
+	esp_err_t ret;
+	esp_err_t err = esp_http_client_perform(client);
+	if (err == ESP_OK) {
+		ESP_LOGD(TAG, "HTTP GET Status = %d, content_length = %d",
+				esp_http_client_get_status_code(client),
+				esp_http_client_get_content_length(client));
+		ESP_LOGD(TAG, "local_response_buffer=%d",  strlen(local_response_buffer));
+		//ESP_LOG_BUFFER_HEX(TAG, local_response_buffer, strlen(local_response_buffer));
+		ESP_LOGI(TAG, "%s",  local_response_buffer);
+
+#if CONFIG_JSON_PARSE
+		ESP_LOGI(TAG, "Deserialize.....");
+		cJSON *root = cJSON_Parse(local_response_buffer);
+		JSON_Parse(root);
+		cJSON_Delete(root);
+#endif
+		ret = ESP_OK;
+	} else {
+		ESP_LOGE(TAG, "HTTP DELETE request failed: %s", esp_err_to_name(err));
+		ret = ESP_FAIL;
+	}
+	esp_http_client_cleanup(client);
+	return ret;
+}
+
+int sqlite3_client_maxid(char * path)
+{
+	ESP_LOGI(TAG, "sqlite3_client_maxid path=%s",path);
+	char local_response_buffer[MAX_HTTP_OUTPUT_BUFFER] = {0};
+	char url[64];
+	sprintf(url, "http://%s:%s/", EXAMPLE_ESP_WEB_SERVER_IP, EXAMPLE_ESP_WEB_SERVER_PORT);
+	if (strlen(path) > 0) {
+		int url_length = strlen(url);
+		if (url[url_length-1] != '/') strcat(url,"/");
+		strcat(url, path);
+	}
+	// SELECT * FROM "customers" ORDER BY "id" desc LIMIT 1;
+	strcat(url, "?limit=1&by=id&order=desc");
+	ESP_LOGI(TAG, "url=%s",url);
+
+	esp_http_client_config_t config = {
+		.url = url,
+		.event_handler = _http_event_handler,
+		.user_data = local_response_buffer,		   // Pass address of local buffer to get response
+	};
+	esp_http_client_handle_t client = esp_http_client_init(&config);
+
+	// GET
+	int rcode = -1;
+	esp_err_t err = esp_http_client_perform(client);
+	if (err == ESP_OK) {
+		ESP_LOGD(TAG, "HTTP GET Status = %d, content_length = %d",
+				esp_http_client_get_status_code(client),
+				esp_http_client_get_content_length(client));
+		ESP_LOGD(TAG, "local_response_buffer=%d",  strlen(local_response_buffer));
+		ESP_LOGD(TAG, "%s",  local_response_buffer);
+
+		ESP_LOGD(TAG, "Deserialize.....");
+		cJSON *root = cJSON_Parse(local_response_buffer);
+		int root_array_size = cJSON_GetArraySize(root); 
+		ESP_LOGD(TAG, "root_array_size=%d", root_array_size);
+		for (int i=0;i<root_array_size;i++) {
+			cJSON *array = cJSON_GetArrayItem(root,i);
+			char *id = cJSON_GetObjectItem(array,"id")->valuestring;
+			ESP_LOGD(TAG, "id=%s",id);
+			rcode = atoi(id);
+		}
+		//JSON_Parse(root);
+		cJSON_Delete(root);
+	} else {
+		ESP_LOGE(TAG, "HTTP GET request failed: %s", esp_err_to_name(err));
+	}
+	esp_http_client_cleanup(client);
+	return rcode;
+}
+
+void http_task(void *pvParameters)
+{
+	// Read all data
+	ESP_LOGI(TAG, "");
+	ESP_LOGI(TAG, "Enter key to Read all data");
+	xEventGroupClearBits(xEventGroup, KEYBOARD_ENTER_BIT);
+	xEventGroupWaitBits(xEventGroup, KEYBOARD_ENTER_BIT, pdFALSE, pdTRUE, portMAX_DELAY);
+	sqlite3_client_get("customers/"); 
+
+	// Read by ID
+	ESP_LOGI(TAG, "");
+	ESP_LOGI(TAG, "Enter key to Read by ID");
+	xEventGroupClearBits(xEventGroup, KEYBOARD_ENTER_BIT);
+	xEventGroupWaitBits(xEventGroup, KEYBOARD_ENTER_BIT, pdFALSE, pdTRUE, portMAX_DELAY);
+	sqlite3_client_get("customers/3"); 
+
+	// Read by gender
+	ESP_LOGI(TAG, "");
+	ESP_LOGI(TAG, "Enter key to Read by gender");
+	xEventGroupClearBits(xEventGroup, KEYBOARD_ENTER_BIT);
+	xEventGroupWaitBits(xEventGroup, KEYBOARD_ENTER_BIT, pdFALSE, pdTRUE, portMAX_DELAY);
+	sqlite3_client_get("customers/gender/2"); 
+
+	//Create
+	ESP_LOGI(TAG, "");
+	ESP_LOGI(TAG, "Enter key to Create new record");
+	xEventGroupClearBits(xEventGroup, KEYBOARD_ENTER_BIT);
+	xEventGroupWaitBits(xEventGroup, KEYBOARD_ENTER_BIT, pdFALSE, pdTRUE, portMAX_DELAY);
+	int newid = 0;
+	char path[64];
+	if (sqlite3_client_post("customers/", "Tom", 1) == ESP_OK) {
+		newid = sqlite3_client_maxid("customers/"); 
+		ESP_LOGI(TAG, "newid=%d", newid);
+		sprintf(path, "customers/%d", newid);
+		sqlite3_client_get(path); 
+	}
+
+	//Update
+	ESP_LOGI(TAG, "");
+	ESP_LOGI(TAG, "Enter key to Update new record");
+	xEventGroupClearBits(xEventGroup, KEYBOARD_ENTER_BIT);
+	xEventGroupWaitBits(xEventGroup, KEYBOARD_ENTER_BIT, pdFALSE, pdTRUE, portMAX_DELAY);
+	sprintf(path, "customers/%d", newid);
+	if(sqlite3_client_get(path) == ESP_OK) { 
+		sqlite3_client_put(path, "Petty", 2); 
+		sqlite3_client_get(path); 
+	}
+
+	//Delete
+	ESP_LOGI(TAG, "");
+	ESP_LOGI(TAG, "Enter key to Delete new record");
+	xEventGroupClearBits(xEventGroup, KEYBOARD_ENTER_BIT);
+	xEventGroupWaitBits(xEventGroup, KEYBOARD_ENTER_BIT, pdFALSE, pdTRUE, portMAX_DELAY);
+	if(sqlite3_client_get(path) == ESP_OK) { 
+		sqlite3_client_delete(path); 
+		sqlite3_client_get("customers"); 
+	}
+
+	while(1) {
+		vTaskDelay(1);
+	}
+
+}
+
+void keyin_task(void *pvParameters)
+{
+	//ESP_LOGI(pcTaskGetTaskName(0), "Start");
+	uint16_t c;
+	while (1) {
+		c = fgetc(stdin);
+		if (c == 0xffff) {
+			vTaskDelay(10);
+			continue;
+		}
+		//ESP_LOGI(pcTaskGetTaskName(0), "c=%x", c);
+		if (c == 0x0a) {
+			ESP_LOGD(pcTaskGetTaskName(0), "Push Enter");
+			xEventGroupSetBits(xEventGroup, KEYBOARD_ENTER_BIT);
+		}
+	}
+}
+
+void app_main()
+{
+	//Initialize NVS
+	esp_err_t ret = nvs_flash_init();
+	if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+	  ESP_ERROR_CHECK(nvs_flash_erase());
+	  ret = nvs_flash_init();
+	}
+	ESP_ERROR_CHECK(ret);
+
+	ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
+	wifi_init_sta();
+
+	//Create EventGroup
+	xEventGroup = xEventGroupCreate();
+
+	//Check everything was created
+	configASSERT( xEventGroup );
+
+	//Start keyboard task
+	xTaskCreate(keyin_task, "KEYIN", 1024*2, NULL, 2, NULL);
+
+	//Start http task
+	xTaskCreate(http_task, "HTTP", 1024*8, NULL, 2, NULL);
+}
